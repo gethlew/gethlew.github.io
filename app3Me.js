5fc75df07852197b560a8b6b35eafc4bb8c1815a
Gethin <Gethin>
2021-04-29 10:49:54 +0100

    Change to previous js file works correctly

--- app3Me.js
+++ app3Me.js
@@ -99,7 +99,7 @@ class App{
         //Material colour
         this.helper.addVisual( groundBody, 0xFFAA00 );
 
-        /*
+        
         const shape = new CANNON.Sphere(0.1);
         this.jointBody = new CANNON.Body({mass: 0});
         this.jointBody.addShape( shape );
@@ -108,7 +108,8 @@ class App{
         //Body will not be affected by other rigid bodies.
         this.jointBody.collisionFilterMask = 0;
         this.world.add( this.jointBody);
-        */
+
+
         this.box = this.addBody();
 
         
