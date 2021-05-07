ece52f29ec1e9881747dcda5e6a17df2e70ebae2
Gethin <Gethin>
2021-05-07 13:18:41 +0100

    Back to knight

--- appKC.js
+++ appKC.js
@@ -61,7 +61,7 @@ class App{
 		// Load a GLTF resource
 		loader.load(
 			// resource URL
-			`RedBellPepper.glb`,
+			`knight2.glb`,
 			// called when the resource is loaded
 			function ( gltf ) {
 				const object = gltf.scene.children[5];
