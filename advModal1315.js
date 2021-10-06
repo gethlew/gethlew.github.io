f7ef895bfa906c36bc9dec6417fb01f3fb6148f1
gethlew <dgethinlewis@gmail.com>
2021-10-06 15:00:33 +0100

    Added more info to first 8 days. Js files for modl

---
+++ advModal1315.js
@@ -0,0 +1,99 @@
+var modalAdv11 = document.getElementById("advModal11");
+
+  var btn = document.getElementById("btn11");
+
+  var span = document.getElementsByClassName("closeADV11")[0];
+
+  btn.onclick = function() {
+    modalAdv11.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modalAdv11.style.display = "none";
+    }
+
+  window.onlick = function(event) {
+    if (event.target == modal) {
+      modalAdv11.style.display = "none";
+    }
+  }
+
+var modalAdv12 = document.getElementById("advModal12");
+
+  var btn = document.getElementById("btn12");
+
+  var span = document.getElementsByClassName("closeADV12")[0];
+
+  btn.onclick = function() {
+    modalAdv12.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modalAdv12.style.display = "none";
+    }
+
+  window.onlick = function(event) {
+    if (event.target == modal) {
+      modalAdv12.style.display = "none";
+    }
+  }
+
+var modalAdv13 = document.getElementById("advModal13");
+
+  var btn = document.getElementById("btn13");
+
+  var span = document.getElementsByClassName("closeADV13")[0];
+
+  btn.onclick = function() {
+    modalAdv13.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modalAdv13.style.display = "none";
+    }
+
+  window.onlick = function(event) {
+    if (event.target == modal) {
+      modalAdv13.style.display = "none";
+    }
+  }
+  
+var modalAdv14 = document.getElementById("advModal14");
+
+  var btn = document.getElementById("btn14");
+
+  var span = document.getElementsByClassName("closeADV14")[0];
+
+  btn.onclick = function() {
+    modalAdv14.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modalAdv14.style.display = "none";
+    }
+
+  window.onlick = function(event) {
+    if (event.target == modal) {
+      modalAdv14.style.display = "none";
+    }
+  }
+
+  var modalAdv5 = document.getElementById("advModal5");
+
+  var btn = document.getElementById("btn5");
+
+  var span = document.getElementsByClassName("closeADV5")[0];
+
+  btn.onclick = function() {
+    modalAdv5.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modalAdv5.style.display = "none";
+    }
+
+  window.onlick = function(event) {
+    if (event.target == modal) {
+      modalAdv5.style.display = "none";
+    }
+  }
